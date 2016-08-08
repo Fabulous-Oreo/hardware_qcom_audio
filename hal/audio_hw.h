a3c6b8193975e2881052903dcbf31905a55a5296
RonGokhale <cips173@gmail.com>
2016-08-08 16:10:27 +0530

    hal: Port FM support from caf hal
    Change-Id: I8339d7d8e6b5991065cb45029863bd8c4dd7b56f

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -97,6 +97,7 @@ enum {
     USECASE_AUDIO_RECORD_LOW_LATENCY,
     USECASE_AUDIO_RECORD_MMAP,
     USECASE_AUDIO_RECORD_HIFI,
+    USECASE_AUDIO_RECORD_FM_VIRTUAL,
 
     /* Voice extension usecases
      *
@@ -135,6 +136,9 @@ enum {
     /* VOIP usecase*/
     USECASE_AUDIO_PLAYBACK_VOIP,
     USECASE_AUDIO_RECORD_VOIP,
+
+    /* FM usecase */
+    USECASE_AUDIO_PLAYBACK_FM,
 
     AUDIO_USECASE_MAX
 };
